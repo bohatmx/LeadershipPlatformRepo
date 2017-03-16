4a54691caea86702a85af781541da9a0e9a6778c
Aubrey Malabie <malengatiger@gmail.com>
2017-03-16 15:43:17 +0200

    initial commit - apps, backend, data model

--- Leadership/leadershiplibrary/src/main/java/com/oneconnectgroup/leadership/library/data/ResponseBag.java
+++ Leadership/leadershiplibrary/src/main/java/com/oneconnectgroup/leadership/library/data/ResponseBag.java
@@ -8,6 +8,25 @@ import java.util.List;
 
 public class ResponseBag {
 
+    private int statusCode;
+    private String message;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
     private List<CategoryDTO> categories;
     private List<DailyThoughtDTO> dailyThoughts;
     private List<EBookDTO> eBooks;
