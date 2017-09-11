c9c89138a2c08cfa13551896c527b6ddd3110f27
B-Chris <nkululekocgs@gmail.com>
2017-09-11 13:01:45 +0200

    added missing icon

--- Leadership001/company/src/main/java/com/ocg/leadership/company/CompanyMainActivity.java
+++ Leadership001/company/src/main/java/com/ocg/leadership/company/CompanyMainActivity.java
@@ -1,14 +1,42 @@
 package com.ocg.leadership.company;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
-public class CompanyMainActivity extends Activity {
+public class CompanyMainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    NavigationView navigationView;
+    Context ctx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
         setContentView(R.layout.activity_company_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ctx = getApplicationContext();
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+     //   navigationView.setNavigationItemSelectedListener(this);
     }
+
+    static final String TAG = CompanyMainActivity.class.getSimpleName();
 
 }
