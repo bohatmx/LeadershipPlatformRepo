2bf115dfbb2676ccf5a6dffa16329699c01f5559
B-Chris <nkululekocgs@gmail.com>
2017-10-24 12:45:47 +0200

    updated full article screen

--- Leadership001/leadershiplibrary/src/main/java/com/oneconnect/leadership/library/activities/FullArticleActivity.java
+++ Leadership001/leadershiplibrary/src/main/java/com/oneconnect/leadership/library/activities/FullArticleActivity.java
@@ -7,16 +7,25 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.oneconnect.leadership.library.R;
 import com.oneconnect.leadership.library.data.NewsDTO;
+import com.oneconnect.leadership.library.data.PhotoDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class FullArticleActivity extends AppCompatActivity {
 
     TextView articleTitleTxt, articleSubTitleTxt, bodyTxt;
     Context ctx;
     NewsDTO newsArticle;
+    ImageView articleImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +35,33 @@ public class FullArticleActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        toolbar.setLogo(R.drawable.harmony);
+        toolbar.setVisibility(View.GONE);
+       // toolbar.setLogo(R.drawable.harmony);
 
         ctx = getApplicationContext();
+        articleImage = (ImageView) findViewById(R.id.articleImage);
 
         if (getIntent().getSerializableExtra("newsArticle") != null) {
                 //  type = ResponseBag.URLS;
                 newsArticle = (NewsDTO) getIntent().getSerializableExtra("newsArticle");
+                if (newsArticle.getPhotos() != null) {
+                    List<PhotoDTO> urlList = new ArrayList<>();
+
+                    Map map = newsArticle.getPhotos();
+                    PhotoDTO vDTO;
+                    String photoUrl;
+                    for (Object value : map.values()) {
+                        vDTO = (PhotoDTO) value;
+                        photoUrl = vDTO.getUrl();
+                        urlList.add(vDTO);
+
+                        Glide.with(ctx)
+                                .load(photoUrl)
+                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .into(articleImage);
+                        //   dvh.captiontxt.setText(vDTO.getCaption());
+                    }
+                }
         }
 
 
--- Leadership001/leadershiplibrary/src/main/java/com/oneconnect/leadership/library/activities/FullPhotoActivity.java
+++ Leadership001/leadershiplibrary/src/main/java/com/oneconnect/leadership/library/activities/FullPhotoActivity.java
@@ -1,11 +1,17 @@
 package com.oneconnect.leadership.library.activities;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -14,22 +20,32 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.oneconnect.leadership.library.R;
 import com.oneconnect.leadership.library.data.PhotoDTO;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class FullPhotoActivity extends AppCompatActivity {
 
     Context ctx;
     ImageView fullPhoto;
     PhotoDTO photo;
+    Bitmap source;
+    float angle=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_photo);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setLogo(R.drawable.harmony);
+        toolbar.setVisibility(View.GONE);
+       // toolbar.setLogo(R.drawable.harmony);
 
         ctx = getApplicationContext();
 
         fullPhoto = (ImageView) findViewById(R.id.fullPhoto);
+
 
         if (getIntent().getSerializableExtra("photo") != null) {
             photo = (PhotoDTO) getIntent().getSerializableExtra("photo");
@@ -38,8 +54,55 @@ public class FullPhotoActivity extends AppCompatActivity {
                     .load(photo.getUrl())
                     .diskCacheStrategy(DiskCacheStrategy.ALL)
                     .into(fullPhoto);
+           // getBitmapFromURL(photo.getUrl());
+           // fullPhoto.setImageBitmap(getBitmapFromURL(photo.getUrl()));
+
+           /* fullPhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    angle+=90;
+                    Bitmap rotatedImage = rotateImage(getBitmapFromURL(photo.getUrl()),angle);
+                    fullPhoto.setImageBitmap(rotatedImage);
+
+                }
+            });*/
         }
+    }
+
+
+
+    /*public static Bitmap getBitmapFromURL(final String src) {
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap myBitmap = null;
+                try {
+                    URL url = new URL(src);
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setDoInput(true);
+                    connection.connect();
+                    InputStream input = connection.getInputStream();
+                    myBitmap = BitmapFactory.decodeStream(input);
+                //    return myBitmap;
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        });
+        thread.start();
+        return myBitmap;
+    }*/
+
+    public static Bitmap rotateImage(Bitmap sourceImage, float angle)
+    {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(sourceImage, 0, 0, sourceImage.getWidth(), sourceImage.getHeight(), matrix, true);
+    }
 
     }
 
-}
+
